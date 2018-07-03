033e18120579cf5593ec855088a00753782eb745
ab.mcl <ab.mcl@metrocast.net>
2018-07-03 19:31:53 -0400

    Update Dev

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,3 @@
-USE scrabbleValues_db;
-
-INSERT INTO letters(
-VALUES(A, 1)
-VALUES(B, 3)
-VALUES(C, 3)
-VALUES(D, 2)
-VALUES(E, 1)
-VALUES(F, 4)
-VALUES(G, 2)
-VALUES(H, 4)
-VALUES(I, 1)
-VALUES(J, 8)
-VALUES(K, 5)
-VALUES(L, 1)
-VALUES(M, 3)
-VALUES(N, 1)
-VALUES(O, 1)
-VALUES(P, 3)
-VALUES(Q, 10)
-VALUES(R, 1)
-VALUES(S, 1)
-VALUES(T, 1)
-VALUES(U, 1)
-VALUES(V, 4)
-VALUES(W, 4)
-VALUES(X, 8)
-VALUES(Y, 4)
-VALUES(Z, 10)
-);
+-- Replace values with desired entities --
+INSERT INTO scores (user_name, round, score)
+VALUES ("Mattieice", 10, 1), ("Mattieice2", 20, 10), ("Mattieice3", 100, 122);
