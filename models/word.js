eea0ec55746cfbec2fbf6c1cd0b7f5219bff1ebe
Molly Zurheide <you@example.com>
2018-07-06 14:14:51 -0400

    updated word.js

--- models/word.js
+++ models/word.js
@@ -1,6 +1,7 @@
-// Creates a "Chirp" model that matches up with DB
-
 var Sequelize = require("sequelize");
+
+// sequelize (lowercase) references my connection to the DB.
+var sequelize = require("../config/connection.js");
 
 // Creates mySQL connection using Sequelize
 var sequelize = new Sequelize("entries", "root", "password", {
@@ -14,91 +15,24 @@ var sequelize = new Sequelize("entries", "root", "password", {
     }
   });
 
-   var entries = sequelize.define("entries", { //all words with length between 4 and 15 letters
-        word: {
-            type: Sequelize.STRING,
-            validate: {
-                len: [4,15]    
-            } 
-        },
-        wordtype: Sequelize.STRING,
-            // Other attributes here
-    });
-
-
-entries.sync({
-	force: true //forces to drop table name if exists
-}).then(function() {
-	return entries.create({
-        country: "Afghanistan",
-        phoneCode: 93,
-        capital: "Kabul",
-        independenceYear: 1919
-    })
-});
-
-
-/*entries.findAll({
-	where: {
-        word: { /////////////////////????????
-            char_length: {
-                min: 4,
-                max: 15
-            }
-        }
+// Creates an "entries" model that matches up with DB
+var entries = sequelize.define("entries", {
+    word: {
+        type: Sequelize.STRING,
+        validate: {
+            len: [4,15]    
+        } 
     },
-    validate: {
-        len: [4,15]
-    }
-})
-
-
-entries.findAll({
-    where: sequelize.where(sequelize.fn("char_length", sequelize.col(word)), 6) //BETWEEN 4 AND 15????
-})
-*/
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
-
-// Creates a "Book" model that matches up with DB
-var Book = sequelize.define("book", {
-  title: Sequelize.STRING,
-  author: Sequelize.STRING,
-  genre: Sequelize.STRING,
-  pages: Sequelize.INTEGER
+    wordtype: Sequelize.STRING
 });
 
 // Syncs with DB
-Book.sync();
+entries.sync();
+
+//pull all matching entries
+entries.findAll({}).then (function(results) {
+    console.log(results);
+});
 
 // Makes the Book Model available for other files (will also create a table)
-module.exports = Book;
-
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-
-connection.query(
-    "SELECT word,wordtype FROM entries WHERE CHAR_LENGTH(word) BETWEEN 4 AND 15",
-    function(err, results) {
-        if (results.indexOf(guessedWord) >= 0) { //if guessed word is found in dictionary...
-            //console.log(results.indexOf(guessedWord));
-            var position = (results.indexOf(guessedWord));
-            //if part of speech of that matching word from dictionary matches randomly generated one...
-            if (randomPOS == results.position.wordtype) { //not sure about this
-                win();
-            }       
-            else {
-                loss();
-            }
-        } else {
-            loss();
-        }
-    });
+module.exports = entries;
